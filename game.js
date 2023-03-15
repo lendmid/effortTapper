4cf0a5caac8b94dd6c817a4d6f4696d004866299
Dmitrii Doroshenko <DmitriyD@radware.com>
2023-03-16 00:12:22 +0200

    transfered bird to pont

--- game.js
+++ game.js
@@ -1,55 +1,50 @@
+console.group();
+
 const RAD = Math.PI / 180;
 const scrn = document.getElementById("canvas");
 const sctx = scrn.getContext("2d");
 scrn.tabIndex = 1;
 
-const sceneHeight = 700
-const sceneWidth = 1400
+const sceneHeight = 500;
+const sceneWidth = 1000;
+const pointRadius = 35;
 
+const drawPoint = (x, y) => {
+  sctx.beginPath();
+  sctx.arc(x, y, pointRadius, 0, 2 * Math.PI);
+  sctx.fillStyle = "#E7E7E7";
+  sctx.fill();
+  sctx.save();
+};
+
+const drawDashedLine = (height) => {
+  sctx.beginPath();
+  sctx.setLineDash([15, 15]);
+  sctx.moveTo(0, height);
+  sctx.lineTo(1000, height);
+  sctx.strokeStyle = "rgba(0, 0, 0, 0.5)";
+  sctx.stroke();
+
+  sctx.save();
+};
 
 scrn.addEventListener("click", () => {
   switch (state.curr) {
     case state.getReady:
-      debugger
       state.curr = state.Play;
-      // SFX.start.play();
       break;
     case state.Play:
-      bird.flap();
+      point.flap();
       break;
     case state.gameOver:
       state.curr = state.getReady;
-      bird.speed = 0;
-      bird.y = 100;
-      pipe.pipes = [];
+      point.speed = 0;
+      point.y = 100;
+      // pipe.pipes = [];
       UI.score.curr = 0;
-      // SFX.played = false;
       break;
   }
 });
-
-// scrn.onkeydown = function keyDown(e) {
-//   if (e.keyCode === 32 || e.keyCode === 87 || e.keyCode === 38) {
-//     // Space Key or W key or arrow up
-//     switch (state.curr) {
-//       case state.getReady:
-//         state.curr = state.Play;
-//         // SFX.start.play();
-//         break;
-//       case state.Play:
-//         bird.flap();
-//         break;
-//       case state.gameOver:
-//         state.curr = state.getReady;
-//         bird.speed = 0;
-//         bird.y = 100;
-//         pipe.pipes = [];
-//         UI.score.curr = 0;
-//         // SFX.played = false;
-//         break;
-//     }
-//   }
-// };
 
 let frames = 0;
 let dx = 2;
@@ -60,181 +55,70 @@ const state = {
   gameOver: 2,
 };
 
-// const SFX = {
-//   start: new Audio(),
-//   flap: new Audio(),
-//   score: new Audio(),
-//   hit: new Audio(),
-//   die: new Audio(),
-//   played: false,
-// };
-
-const gnd = {
-  sprite: new Image(),
+const sibling = {
+  // sprite: new Image(),
   x: 0,
-  y: 0,
+  y: 100,
   draw: function () {
+    // sctx.save();
+    drawDashedLine(this.y);
     // console.log("scrn.height - this.sprite.height: ", scrn.height - this.sprite.height)
     // this.y = parseFloat(scrn.height - this.sprite.height);
     // sctx.drawImage(this.sprite, this.x, this.y);
-    const fixTaxLevel = sceneHeight * 0.3;
+    // const fixTaxLevel = sceneHeight * 0.3;
     // sctx.beginPath();
-    this.y = sceneHeight - fixTaxLevel
-    sctx.save();
-    sctx.fillStyle = "#FF000020";
-    sctx.fillRect(0, sceneHeight - fixTaxLevel, sceneWidth, fixTaxLevel);
+    // this.y = sceneHeight - fixTaxLevel
+    // sctx.save();
+    // sctx.fillStyle = "#FF000020";
+    // sctx.fillRect(0, sceneHeight - fixTaxLevel, sceneWidth, fixTaxLevel);
     // sctx.stroke();
   },
-  update: function () {
-    if (state.curr !== state.Play) return;
-    debugger
-    this.x -= dx;
-    this.x = this.x % (this.sprite.width / 2);
-  },
+  // update: function () {
+  //   if (state.curr !== state.Play) return;
+  //   debugger
+  //   this.x -= dx;
+  //   this.x = this.x % (this.sprite.width / 2);
+  // },
 };
 
-// const bg = {
-//   sprite: new Image(),
-//   x: 0,
-//   y: 0,
-//   draw: function () {
-//     y = parseFloat(scrn.height - this.sprite.height);
-//     sctx.drawImage(this.sprite, this.x, y);
-//   },
-// };
-
-const pipe = {
-  top: { sprite: new Image() },
-  bot: { sprite: new Image() },
-  gap: 85,
-  moved: true,
-  pipes: [],
+const floor = {
+  // sprite: new Image(),
+  x: 0,
+  y: 600,
   draw: function () {
-    for (let i = 0; i < this.pipes.length; i++) {
-      let p = this.pipes[i];
-      sctx.drawImage(this.top.sprite, p.x, p.y);
-      sctx.drawImage(
-          this.bot.sprite,
-          p.x,
-          p.y + parseFloat(this.top.sprite.height) + this.gap
-      );
-    }
-  },
-  update: function () {
-    if (state.curr != state.Play) return;
-    if (frames % 100 == 0) {
-      this.pipes.push({
-        x: parseFloat(scrn.width),
-        y: -210 * Math.min(Math.random() + 1, 1.8),
-      });
-    }
-    this.pipes.forEach((pipe) => {
-      pipe.x -= dx;
-    });
-
-    if (this.pipes.length && this.pipes[0].x < -this.top.sprite.width) {
-      this.pipes.shift();
-      this.moved = true;
-    }
+    drawDashedLine(this.y);
   },
 };
 
-const bird = {
-  animations: [
-    { sprite: new Image() },
-    { sprite: new Image() },
-    { sprite: new Image() },
-    { sprite: new Image() },
-  ],
-  rotatation: 0,
-  x: 50,
-  y: 100,
+const point = {
+  x: 250,
+  y: 350,
   speed: 0,
-  gravity: 0.05,
-  // gravity: 0.125,
+  gravity: 0.125, //0.05
   thrust: 3.6,
   frame: 0,
   draw: function () {
-    let h = this.animations[this.frame].sprite.height;
-    let w = this.animations[this.frame].sprite.width;
-    sctx.save();
-    sctx.translate(this.x, this.y);
-    sctx.rotate(this.rotatation * RAD);
-    sctx.drawImage(this.animations[this.frame].sprite, -w / 2, -h / 2);
-    sctx.restore();
-  },
-  update: function () {
-    let r = parseFloat(this.animations[0].sprite.width) / 2;
-    switch (state.curr) {
-      case state.getReady:
-        this.rotatation = 0;
-        this.y += frames % 10 == 0 ? Math.sin(frames * RAD) : 0;
-        this.frame += frames % 10 == 0 ? 1 : 0;
-        break;
-      case state.Play:
-        this.frame += frames % 5 == 0 ? 1 : 0;
-        this.y += this.speed;
-        this.setRotation();
-        this.speed += this.gravity;
-        if (this.y + r >= gnd.y || this.collisioned()) {
-          debugger
-          state.curr = state.gameOver;
-        }
-
-        break;
-      case state.gameOver:
-        this.frame = 1;
-        if (this.y + r < gnd.y) {
-          this.y += this.speed;
-          this.setRotation();
-          this.speed += this.gravity * 2;
-        } else {
-          this.speed = 0;
-          this.y = gnd.y - r;
-          this.rotatation = 90;
-          // if (!SFX.played) {
-          //   SFX.die.play();
-          //   SFX.played = true;
-          // }
-        }
-
-        break;
+    const isFloorIntersection = this.y + pointRadius > floor.y;
+    // sctx.save();
+    drawPoint(this.x, this.y);
+    if (!isFloorIntersection) {
+      this.speed += this.gravity;
     }
-    this.frame = this.frame % this.animations.length;
+    this.y += this.speed;
+    // this.y += this.speed;
+    // sctx.restore();
+    console.log("point: ", this);
+    // let h = this.animations[this.frame].sprite.height;
+    // let w = this.animations[this.frame].sprite.width;
+    // sctx.save();
+    // sctx.translate(this.x, this.y);
+    // sctx.rotate(this.rotatation * RAD);
+    // sctx.drawImage(this.animations[this.frame].sprite, -w / 2, -h / 2);
+    // sctx.restore();
   },
   flap: function () {
     if (this.y > 0) {
-      // SFX.flap.play();
       this.speed = -this.thrust;
-    }
-  },
-  setRotation: function () {
-    if (this.speed <= 0) {
-      this.rotatation = Math.max(-25, (-25 * this.speed) / (-1 * this.thrust));
-    } else if (this.speed > 0) {
-      this.rotatation = Math.min(90, (90 * this.speed) / (this.thrust * 2));
-    }
-  },
-  collisioned: function () {
-    if (!pipe.pipes.length) return;
-    let bird = this.animations[0].sprite;
-    let x = pipe.pipes[0].x;
-    let y = pipe.pipes[0].y;
-    let r = bird.height / 4 + bird.width / 4;
-    let roof = y + parseFloat(pipe.top.sprite.height);
-    let floor = roof + pipe.gap;
-    let w = parseFloat(pipe.top.sprite.width);
-    if (this.x + r >= x) {
-      if (this.x + r < x + w) {
-        if (this.y - r <= roof || this.y + r >= floor) {
-          // SFX.hit.play();
-          return true;
-        }
-      } else if (pipe.moved) {
-        UI.score.curr++;
-        // SFX.score.play();
-        pipe.moved = false;
-      }
     }
   },
 };
@@ -259,7 +143,7 @@ const UI = {
         this.x = parseFloat(scrn.width - this.getReady.sprite.width) / 2;
         this.tx = parseFloat(scrn.width - this.tap[0].sprite.width) / 2;
         this.ty =
-            this.y + this.getReady.sprite.height - this.tap[0].sprite.height;
+          this.y + this.getReady.sprite.height - this.tap[0].sprite.height;
         sctx.drawImage(this.getReady.sprite, this.x, this.y);
         sctx.drawImage(this.tap[this.frame].sprite, this.tx, this.ty);
         break;
@@ -268,7 +152,7 @@ const UI = {
         this.x = parseFloat(scrn.width - this.gameOver.sprite.width) / 2;
         this.tx = parseFloat(scrn.width - this.tap[0].sprite.width) / 2;
         this.ty =
-            this.y + this.gameOver.sprite.height - this.tap[0].sprite.height;
+          this.y + this.gameOver.sprite.height - this.tap[0].sprite.height;
         sctx.drawImage(this.gameOver.sprite, this.x, this.y);
         sctx.drawImage(this.tap[this.frame].sprite, this.tx, this.ty);
         break;
@@ -291,8 +175,8 @@ const UI = {
         let sc = `SCORE :     ${this.score.curr}`;
         try {
           this.score.best = Math.max(
-              this.score.curr,
-              localStorage.getItem("best")
+            this.score.curr,
+            localStorage.getItem("best")
           );
           localStorage.setItem("best", this.score.best);
           let bs = `BEST  :     ${this.score.best}`;
@@ -315,51 +199,27 @@ const UI = {
   },
 };
 
-// gnd.sprite.src = "img/ground.png";
-// bg.sprite.src = "img/BG.png";
-pipe.top.sprite.src = "img/toppipe.png";
-pipe.bot.sprite.src = "img/botpipe.png";
 UI.gameOver.sprite.src = "img/go.png";
 UI.getReady.sprite.src = "img/getready.png";
 UI.tap[0].sprite.src = "img/tap/t0.png";
 UI.tap[1].sprite.src = "img/tap/t1.png";
-bird.animations[0].sprite.src = "img/bird/b0.png";
-bird.animations[1].sprite.src = "img/bird/b1.png";
-bird.animations[2].sprite.src = "img/bird/b2.png";
-bird.animations[3].sprite.src = "img/bird/b0.png";
-// SFX.start.src = "sfx/start.wav";
-// SFX.flap.src = "sfx/flap.wav";
-// SFX.score.src = "sfx/score.wav";
-// SFX.hit.src = "sfx/hit.wav";
-// SFX.die.src = "sfx/die.wav";
-
-
-function update() {
-  bird.update();
-  gnd.draw();
-  // gnd.update();
-  pipe.update();
-  UI.update();
-}
 
 function draw() {
   sctx.fillStyle = "#fff";
-  // sctx.fillStyle = "#30c0df";
   sctx.fillRect(0, 0, scrn.width, scrn.height);
-  // bg.draw();
-  pipe.draw();
 
-  gnd.draw();
-  bird.draw();
+  sibling.draw();
+  floor.draw();
+  point.draw();
   UI.draw();
 }
 
 function gameLoop() {
-  update();
   draw();
   frames++;
-  console.log("Updated. Frames: ", frames)
-  console.log("state: ", state)
+  // console.log("state: ", state);
 }
 
 setInterval(gameLoop, 20);
+
+console.groupEnd();
